f2a81901cef34770b76badfbcd7618556cb0c549
haoyang9804 <haoyanghkust@gmail.com>
2022-10-17 17:03:32 +0800

    update forall-play.hs

--- Haskell/forall-play.hs
+++ Haskell/forall-play.hs
@@ -35,6 +35,26 @@ bar = undefined
 
 ---------- This is an ExistentialQuantification example
 
+-- data Showable = forall s. (Show s) => Showable s
 data Showable = forall s. (Show s) => Showable s
 
+someShowables :: [Showable]
+someShowables = [Showable "Hi", Showable 5, Showable (1, 2)]
 
+printShowables :: [Showable] -> IO ()
+printShowables = mapM_ (\(Showable s) -> print s)
+
+main :: IO ()
+main = printShowables someShowables
+
+-- In this example, we create a heterogeneous list
+-- containing diverse Showable contents
+
+-- The noteworthy thing is `forall` here is like an
+-- existential quantifier
+
+-- If we define `Showable` in regular way without forall,
+-- then it looks like
+--      data Showable s = Show s => Showable s
+-- by which we cannot define `someShowables` since
+-- `Showable` requires a type parameter.
