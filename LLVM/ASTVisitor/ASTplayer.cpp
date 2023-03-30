f18c92c14ff852988c0d54ee53ea5a96dffbcf4a
haoyang9804 <haoyanghkust@gmail.com>
2023-03-30 21:36:21 +0800

    update ASTplayer.cpp

--- LLVM/ASTVisitor/ASTplayer.cpp
+++ LLVM/ASTVisitor/ASTplayer.cpp
@@ -4,6 +4,7 @@
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
 #include "clang/Basic/SourceLocation.h"
+#include "clang/Basic/FileEntry.h"
 #include "FileReader.hpp"
 #include "clang/Rewrite/Core/Rewriter.h"
 
@@ -13,8 +14,8 @@ using namespace clang;
 class FindNamedClassVisitor
   : public RecursiveASTVisitor<FindNamedClassVisitor> {
 public:
-  explicit FindNamedClassVisitor(Rewriter& rewriter, ASTContext *Context)
-    : rewriter(rewriter), Context(Context) {}
+  explicit FindNamedClassVisitor(Rewriter& rewriter, ASTContext *Context, SourceManager& SM)
+    : rewriter(rewriter), Context(Context), SM(SM) {}
 
   bool VisitCXXRecordDecl(CXXRecordDecl *Declaration) {
     if (Declaration->getQualifiedNameAsString() == "n::m::C") {
@@ -58,14 +59,23 @@ public:
   bool VisitFunctionDecl(FunctionDecl *Declaration) {
     if (Declaration->getQualifiedNameAsString() == "foo") {
 
+      // SourceRange funcRange = Declaration->getSourceRange();
+
+      // IdentifierInfo* barId = &Context->Idents.get("bar");
+      // DeclarationName barName(barId);
+      // Declaration->setDeclName(barName);
+      DeclarationNameInfo nameinfo = Declaration->getNameInfo();
+      SourceLocation nameloc = nameinfo.getLoc();
+      llvm::errs() << "func foo name loc is " << nameloc.printToString(SM) << "\n";
       SourceRange funcRange = Declaration->getSourceRange();
-
-
-      IdentifierInfo* barId = &Context->Idents.get("bar");
-      DeclarationName barName(barId);
-      Declaration->setDeclName(barName);
-      rewriter.ReplaceText(Declaration->getSourceRange(), "bar");
-      llvm::errs() << "hello\n";  
+      SourceLocation locBegin = funcRange.getBegin();
+      SourceLocation locEnd = funcRange.getEnd();
+      llvm::errs() << "func foo loc begin at " << locBegin.printToString(SM) << "\n";
+      llvm::errs() << "func foo loc end at " << locEnd.printToString(SM) << "\n";
+      llvm::errs() << "func name length is " << Declaration->getQualifiedNameAsString().size() << "\n";
+      bool rewrite_result = rewriter.ReplaceText(nameloc, Declaration->getQualifiedNameAsString().size(), llvm::StringRef("barr"));
+      // Don't know why... returning true means fail but returning false means succeeds
+      llvm::errs() << "Rewriting foo into bar " << (rewrite_result ? "fails" : "succeeds") << "\n";
     }
     return true;
   }
@@ -73,12 +83,13 @@ public:
 private:
   ASTContext *Context;
   Rewriter &rewriter;
+  SourceManager &SM;
 };
 
 class FindNamedClassConsumer : public clang::ASTConsumer {
 public:
-  explicit FindNamedClassConsumer(Rewriter rewriter, ASTContext *Context)
-    : Visitor(rewriter, Context) {}
+  explicit FindNamedClassConsumer(Rewriter& rewriter, ASTContext *Context, SourceManager& SM)
+    : Visitor(rewriter, Context, SM) {}
 
   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
@@ -91,13 +102,19 @@ class FindNamedClassAction : public clang::ASTFrontendAction {
 public:
   std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
     clang::CompilerInstance &Compiler, llvm::StringRef InFile) override {
+    
     rewriter.setSourceMgr(Compiler.getSourceManager(), Compiler.getLangOpts());
+
     return std::unique_ptr<clang::ASTConsumer>(
-        new FindNamedClassConsumer(rewriter, &Compiler.getASTContext()));
+        new FindNamedClassConsumer(rewriter, &Compiler.getASTContext(), Compiler.getSourceManager()));
   }
 
   void EndSourceFileAction() override {
-    rewriter.overwriteChangedFiles();
+    llvm::errs() << "Here goes EndSourceFileAction\n";
+    bool rewriterChangeFileResult = rewriter.overwriteChangedFiles();
+    // Don't know why... returning true means fail but returning false means succeeds
+    // And don't know why again. Clang will create a new file named ``input.cc'' in build folder
+    llvm::errs() << "overwriteChangedFiles " << (rewriterChangeFileResult ? "fails" : "succeeds") << "\n";
   }
 private:
   Rewriter rewriter;
